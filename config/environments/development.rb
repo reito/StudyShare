de454b36c7dc9cde130f0c79ff1f4c08f4cb620e
reito <zhenlinshannei@gmail.com>
2020-08-28 20:14:53 +0900

    Add user following

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,9 +45,9 @@ Rails.application.configure do
     domain: 'gmail.com',
     port:587,
     #gmailのユーザアカウント（xxxx@gmail.com)※念のため、credentials.yml.enc行き
-    user_name: Rails.application.credentials.gmail[:user_name],
+    user_name: 'zhenlinshannei@gmail.com',
     #gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ずcredentials.yml.endに設定を！！
-    password: Rails.application.credentials.gmail[:password],
+    password: 'vkenkixzooxqzbey',
     #パスワードをBase64でエンコード
     authentication: :login
   }
