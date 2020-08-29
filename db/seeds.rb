553eecf15263fc1c40ab2996089fb192fc69f980
reito <zhenlinshannei@gmail.com>
2020-08-30 01:24:33 +0900

    Add optional true

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,8 @@ end
 users = User.order(:created_at).take(6)
 50.times do
     content = Faker::Lorem.sentence(5)
-    users.each { |user| user.microposts.create!(content: content) }
+    users.each { |user| user.microposts.create!(content: content,
+                                                subject: "1") }
 end
 
 # リレーションシップ
