5ee07587175bc120a8dd3078caee5dcc16f5fa08
reito <zhenlinshannei@gmail.com>
2020-08-31 13:39:01 +0900

    Create micropost show

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  before_action :logged_in_user, only: [:english, :math, :physics, :chemistry]
+
   def home
     if logged_in?
       @micropost = current_user.microposts.build
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
