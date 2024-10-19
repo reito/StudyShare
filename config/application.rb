414ecd8745a8f39147708898a0a9fdba224ffe5c
reito <zhenlinshannei@gmail.com>
2024-10-20 02:59:00 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module StudyShareApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.initialize_on_precompile = false
 
     # 認証トークンをremoteに埋め込む
     config.action_view.embed_authenticity_token_in_remote_forms = true
